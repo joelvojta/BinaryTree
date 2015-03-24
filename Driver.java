a9984e8ae1bec8b1c88a16032c38be9fa64e7120
Joel Vojta <joel.vojta@cuw.edu>
2015-03-24 15:55:21 -0500

    Initial Commit

---
+++ Driver.java
@@ -0,0 +1,16 @@
+public class Driver 
+{
+	public static void main(String[] args) 
+	{
+		BinaryTree bt = new BinaryTree();
+		bt.add(2);
+		bt.add(5);
+		bt.add(7);
+		bt.add(1);
+		bt.add(0);
+		bt.add(3);
+		bt.displayInOrder();
+		bt.displayPostOrder();
+
+	}
+}
