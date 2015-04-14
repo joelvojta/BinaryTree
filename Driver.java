53939c9a1d857a03f29fd1c628bc611a9b13e626
joelvojta <joel.vojta@cuw.edu>
2015-04-13 22:02:32 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -5,13 +5,21 @@ public class Driver
 	public static void main(String[] args) 
 	{
 		Random r = new Random();
-		BinaryTree bt = new BinaryTree();  
-		for(int i = 0; i < 5000; i++)
+		BinaryTree bt = new BinaryTree();
+		/*for(int i = 0; i < 5000; i++)
 		{
 			bt.add(r.nextInt());
 		}
 		System.out.print(bt.isBalanced());
+		*/
+		bt.add(2);
+		bt.add(4);
+		bt.add(7);
+		bt.add(8);
+		bt.add(10);
+		bt.add(12);
+		bt.add(14);
+		bt.displayInOrder();
+		System.out.println(bt.isBalanced());
 	}
 }
-
-
